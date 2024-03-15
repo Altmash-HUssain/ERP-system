3f0137c4736647654295df1b2de5f8db7dc73e66
Md Altmash Hussain <160114480+Altmash-HUssain@users.noreply.github.com>
2024-03-15 06:00:47 +0530

    Add files via upload

---
+++ OrdersManagement.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { orders } from '../data';
+
+const OrdersManagement = () => {
+    return (
+        <div>
+            <h1>Orders Management</h1>
+            <ul>
+                {orders.map(order => (
+                    <li key={order.id}>{order.id} - {order.customerId}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default OrdersManagement;
---
+++ ProductsManagement.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { products } from '../data';
+
+const ProductsManagement = () => {
+    return (
+        <div>
+            <h1>Products Management</h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>{product.name} - {product.category}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ProductsManagement;
